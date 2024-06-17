87c22a76816ee0c0fb327e91395b3c8894bb50b2
SSSilviaa <116482510+SSSilviaa@users.noreply.github.com>
2024-06-17 15:04:18 -0400

    Add files via upload

--- StoreAnalog.ino
+++ StoreAnalog.ino
@@ -1,15 +1,20 @@
 #include <SPI.h>
 #include <SD.h>
 
-#define CS_PIN_SD 10
-#define ANALOG_PIN A0
-#define NUM_READINGS 1000 // Number of readings to store
+#define CS_PIN_SD 4
+#define ANALOG_PIN A1
+#define NUM_READINGS 100 // Number of readings to store
+
+volatile bool sampleReady = false;
+int samples[NUM_READINGS];
+int sampleIndex = 0;
 
 File dataFile;
 
 void setup() {
   Serial.begin(9600);
   pinMode(CS_PIN_SD, OUTPUT);
+  pinMode(ANALOG_PIN, INPUT);
 
   // Initialize SD card
   if (!SD.begin(CS_PIN_SD)) {
@@ -18,11 +23,6 @@ void setup() {
   }
   Serial.println("SD Card initialized.");
 
-  // Delete the existing file if it exists
-  if (SD.exists("data.txt")) {
-    SD.remove("data.txt");
-  }
-
   // Open file for writing
   dataFile = SD.open("data.txt", FILE_WRITE);
   if (!dataFile) {
@@ -30,39 +30,46 @@ void setup() {
     return;
   }
 
-  // Configure ADC prescaler to 16
-  ADCSRA &= ~(1 << ADPS2 | 1 << ADPS1 | 1 << ADPS0); // Clear prescaler bits
-  ADCSRA |= (1 << ADPS2); // Set prescaler to 16 (ADC clock = 1MHz)
+  // Configure ADC prescaler to 32 (ADC clock = 500kHz)
+  // ADCSRA &= ~(1 << ADPS2 | 1 << ADPS1 | 1 << ADPS0); // Clear prescaler bits
+  // ADCSRA |= (1 << ADPS2) | (1 << ADPS0); // Set prescaler to 32
 
-  Serial.println("ADC configured for maximum speed.");
+  // ADCSRA &= ~(1 << ADPS2 | 1 << ADPS1 | 1 << ADPS0); // Clear prescaler bits
+  // ADCSRA |= (1 << ADPS2); // Set prescaler to 16 (ADC clock = 1MHz)
 
-  // Read analog values and store in SD card
-  for (int i = 0; i < NUM_READINGS; i++) {
-    int sensorValue = i; // fastAnalogRead(ANALOG_PIN); //analogRead(ANALOG_PIN);
-    dataFile.println(sensorValue);
-    Serial.println(sensorValue);
-    delayMicroseconds(1); // Adjust delay as needed
-  }
+  // Configure Timer1 for 2kHz sampling rate (500 microseconds interval)
+  noInterrupts(); // Disable all interrupts
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 0;
+  OCR1A = 56999; // (16*10^6) / (2*10^3) - 1 (must be <65536)
+  TCCR1B |= (1 << WGM12); // CTC mode
+  TCCR1B |= (1 << CS10); // No prescaler
+  TIMSK1 |= (1 << OCIE1A); // Enable timer compare interrupt
+  interrupts(); // Enable all interrupts
+}
 
-  // Close the file
-  dataFile.close();
-  Serial.println("Data logging complete.");
+ISR(TIMER1_COMPA_vect) {
+  sampleReady = true;
 }
 
 void loop() {
-  // Nothing to do here
-}
+  if (sampleReady) {
+    sampleReady = false;
 
-int fastAnalogRead(uint8_t pin) {
-  // Configure ADC mux to select the analog pin
-  ADMUX = (1 << REFS0) | (pin & 0x07);
-
-  // Start the conversion
-  ADCSRA |= (1 << ADSC);
-
-  // Wait for the conversion to complete
-  while (ADCSRA & (1 << ADSC));
-
-  // Return the ADC value
-  return ADC;
+    if (sampleIndex < NUM_READINGS) {
+      int sensorValue = analogRead(ANALOG_PIN);
+      samples[sampleIndex++] = sensorValue;
+    } else {
+      // Write samples to SD card
+      for (int i = 0; i < NUM_READINGS; i++) {
+        // dataFile.println(samples[i]);
+        Serial.println(samples[i]); // Optionally print to Serial Monitor
+      }
+      dataFile.close();
+      Serial.println("Data logging complete.");
+      while (true) {} // Stop the loop
+      // sampleIndex = 0;
+    }
+  }
 }
